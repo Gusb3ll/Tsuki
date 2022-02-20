5dc89996e8e96c33672d3e0b0be071b8bf1519d1
Kitpipat Jaritwong <77166960+Gusb3ll@users.noreply.github.com>
2022-02-20 14:36:07 +0700

    [+] automatic package installation

--- init.bat
+++ init.bat
@@ -13,3 +13,29 @@ mkdir .\modules\DeepCreamPy\decensor_output
 mkdir .\modules\HentAI\input
 mkdir .\modules\HentAI\output\
 mkdir .\modules\HentAI\output\decensor_input
+
+python -m pip install virtualenv
+
+cd modules 
+
+cd Convert
+virtualenv env
+.\env\Scripts\python.exe .\env\Scripts\pip.exe install -r ./requirements.txt
+
+cd ..
+
+cd DeepCreamPy
+virtualenv env
+.\env\Scripts\python.exe .\env\Scripts\pip.exe install -r ./requirements.txt
+
+cd ..
+
+cd HentAI
+virtualenv env
+.\env\Scripts\python.exe .\env\Scripts\pip.exe install -r ./requirements.txt
+
+cd ..
+
+cd ToneRemover
+virtualenv env
+.\env\Scripts\python.exe .\env\Scripts\pip.exe install -r ./requirements.txt
